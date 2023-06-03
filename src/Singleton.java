eac09fe6ca5a1149fd7eb6f98d112d721d6ac4c5
ywalakamar <barxwells@gmail.com>
2023-06-03 12:04:42 +0300

    singleton: lazy initialization

--- src/Singleton.java
+++ src/Singleton.java
@@ -3,13 +3,16 @@
 
 public class Singleton {
 
-    private static final Singleton instance = new Singleton();
+    private static Singleton instance;
 
     // private constructor to prevent client applications using the constructor
     private Singleton() {
     }
 
     public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
         return instance;
     }
 }
